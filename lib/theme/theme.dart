4d86b916928b17aae32a1a9271d2de106479ca51
Pavi87 <pavimanoharan87@gmail.com>
2021-09-13 18:54:07 +0530

    new

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 final backgroundcolor = Color(0xff141629);
 final blue = Color(0xff6C6AEB);
+final blue1 = Color(0xff8684F5);
 final white = Color(0xffffffff);
 final button = Color(0xff282C4A);
+final orange = Color(0xffFF8C00);
