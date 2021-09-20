11feeeb647048c03386bcf5621585a8ad909e680
GOPINATHAN A R <gopiarofficial@gmail.com>
2021-09-20 15:33:49 +0530

    challenge pages mod 1

---
+++ lib/wallet_screens/wallet_chellenge_final.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:momerlin/data/localstorage/userdata_source.dart';
+import 'package:momerlin/theme/theme.dart';
+import 'package:momerlin/wallet_screens/wallet_challenges.dart';
+import 'package:momerlin/walletcreation.dart/wallethome.dart';
+
+class ChellengeFinal extends StatefulWidget {
+  const ChellengeFinal({Key key}) : super(key: key);
+
+  @override
+  _ChellengeFinalState createState() => _ChellengeFinalState();
+}
+
+class _ChellengeFinalState extends State<ChellengeFinal> {
+  var userLanguage, lang = [];
+  @override
+  void initState() {
+    super.initState();
+    getUserLanguage();
+  }
+
+  // ignore: todo
+  //TODO :languagestart
+  Future<void> getUserLanguage() async {
+    lang = await UserDataSource().getLanguage();
+    if (lang.length != null && lang.length != 0) {
+      userLanguage = lang[0];
+    }
+  }
+  // ignore: todo
+  //TODO: LanguageEnd
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundcolor,
+      appBar: AppBar(
+        backgroundColor: backgroundcolor,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Container(
+              // height: 50,
+              // width: 50,
+              color: button,
+              child: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  )),
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        // padding: EdgeInsets.only(left: 20, right: 20, top: 270),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 100),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: button,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 100,
+                        ),
+                        Text(
+                          (lang.length != null &&
+                                  lang.length != 0 &&
+                                  userLanguage['justlikemagicyourealldone'] !=
+                                      null)
+                              ? "${userLanguage['justlikemagicyourealldone']}"
+                              : "Let the challenge \nbegin!",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              letterSpacing: 1,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => WalletChallenges()));
+                          },
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            height: MediaQuery.of(context).size.height * 0.08,
+                            padding: EdgeInsets.only(left: 20),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: blue1),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  (lang.length != null &&
+                                          lang.length != 0 &&
+                                          userLanguage['sendmore'] != null)
+                                      ? "${userLanguage['sendmore']}"
+                                      : "GO HOME",
+                                  style: GoogleFonts.poppins(
+                                      color: white,
+                                      letterSpacing: 1,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                              ],
+                              // ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => WalletHomePage()));
+                          },
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            height: MediaQuery.of(context).size.height * 0.05,
+                            padding: EdgeInsets.only(left: 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  (lang.length != null &&
+                                          lang.length != 0 &&
+                                          userLanguage['gohome'] != null)
+                                      ? "${userLanguage['gohome']}"
+                                      : "CREATE ANOTHER",
+                                  style: GoogleFonts.poppins(
+                                      color: white,
+                                      letterSpacing: 1,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  width: 20,
+                                ),
+                              ],
+                              // ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 130,
+                  top: 65,
+                  child: Image.asset("assets/images/chellenge.png"),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/wallet_screens/wallet_creating_challenge.dart
+++ lib/wallet_screens/wallet_creating_challenge.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:momerlin/data/localstorage/userdata_source.dart';
 import 'package:momerlin/theme/theme.dart';
+import 'package:momerlin/wallet_screens/wallet_chellenge_final.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class WalletCreatingChallenge extends StatefulWidget {
@@ -114,6 +115,8 @@ class _WalletCreatingChallengeState extends State<WalletCreatingChallenge> {
                   routes: {
                     '/W1': (context) => WidgetOne(),
                     '/W2': (context) => WidgetTwo(),
+                    '/W3': (context) => WidgetThree(),
+                    '/W4': (context) => WidgetFour(),
                   }))
         ],
       ),
@@ -336,12 +339,398 @@ class WidgetTwo extends StatefulWidget {
 }
 
 class _WidgetTwoState extends State<WidgetTwo> {
+  bool ischeckvisible = false;
+  var checkColor = blue1;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: () {
-          Navigator.pushNamed(context, '/W1');
-        },
-        child: Container(color: Colors.pink));
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.6,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: backgroundcolor,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.1,
+                width: MediaQuery.of(context).size.width,
+                //color: Colors.red,
+                child: Center(
+                  child: RichText(
+                      text: TextSpan(children: [
+                    TextSpan(
+                        text: 'How many',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        )),
+                    TextSpan(
+                        text: ' SATS ',
+                        style: GoogleFonts.poppins(
+                          color: Colors.orange,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        )),
+                    TextSpan(
+                        text: 'would \n       you like to wager?',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        )),
+                  ])),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 50),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.13,
+            width: MediaQuery.of(context).size.width,
+            //color: Colors.amber,
+            child: Image.asset(
+              "assets/images/bar1.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.08,
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/W3');
+                },
+                color: blue.withOpacity(0.3),
+                child: Text(
+                  "NEXT",
+                  style: GoogleFonts.poppins(
+                    color: blue1,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )),
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class WidgetThree extends StatefulWidget {
+  const WidgetThree({Key key}) : super(key: key);
+
+  @override
+  _WidgetThreeState createState() => _WidgetThreeState();
+}
+
+class _WidgetThreeState extends State<WidgetThree> {
+  bool ischeckvisible = false;
+  var checkColor = blue1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.6,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: backgroundcolor,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.1,
+                width: MediaQuery.of(context).size.width,
+                //color: Colors.red,
+                child: Center(
+                  child: Text(
+                    "What type of \n  challenge?",
+                    style: GoogleFonts.poppins(
+                        decoration: TextDecoration.none,
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    ischeckvisible = true;
+                  });
+                },
+                onDoubleTap: () {
+                  setState(() {
+                    ischeckvisible = false;
+                  });
+                },
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.26,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                      color: (ischeckvisible = true) ? blue1 : button,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: InkWell(
+                          onTap: () {
+                            // setState(() {
+                            //   ischeckvisible = true;
+                            // });
+                          },
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: Container(
+                              height: 35,
+                              width: 35,
+                              color: backgroundcolor,
+                              child: Visibility(
+                                visible: ischeckvisible,
+                                child: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        ischeckvisible = false;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.check,
+                                      color: blue1,
+                                      size: 20,
+                                    )),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 12),
+                        child: Container(
+                            height: 80,
+                            width: 50,
+                            //color: Colors.red,
+                            child: Image.asset(
+                              "assets/images/streak.png",
+                              fit: BoxFit.fill,
+                            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 12),
+                        child: Text(
+                          "STREAK",
+                          style: GoogleFonts.poppins(
+                              decoration: TextDecoration.none,
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.26,
+                width: MediaQuery.of(context).size.width * 0.4,
+                decoration: BoxDecoration(
+                    color: (ischeckvisible == false) ? blue1 : button,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Container(
+                        height: 35,
+                        width: 100,
+                        decoration: BoxDecoration(
+                            color: blue1,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Visibility(
+                            visible: ischeckvisible,
+                            child: Center(
+                              child: Text("COMMING SOON",
+                                  style: GoogleFonts.poppins(
+                                      decoration: TextDecoration.none,
+                                      color: Colors.white,
+                                      fontSize: 8,
+                                      fontWeight: FontWeight.w600)),
+                            )),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 12),
+                      child: Container(
+                          height: 80,
+                          width: 50,
+                          //color: Colors.red,
+                          child: Opacity(
+                            opacity: 0.25,
+                            child: Image.asset(
+                              "assets/images/speed.png",
+                              fit: BoxFit.fill,
+                            ),
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 12),
+                      child: Text(
+                        "SPEED",
+                        style: GoogleFonts.poppins(
+                            decoration: TextDecoration.none,
+                            color: Colors.white.withOpacity(0.2),
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.08,
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/W4');
+                },
+                color: blue.withOpacity(0.3),
+                child: Text(
+                  "NEXT",
+                  style: GoogleFonts.poppins(
+                    color: blue1,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )),
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class WidgetFour extends StatefulWidget {
+  const WidgetFour({Key key}) : super(key: key);
+
+  @override
+  _WidgetFourState createState() => _WidgetFourState();
+}
+
+class _WidgetFourState extends State<WidgetFour> {
+  bool ischeckvisible = false;
+  var checkColor = blue1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.6,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: backgroundcolor,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.1,
+                width: MediaQuery.of(context).size.width,
+                //color: Colors.red,
+                child: Center(
+                  child: RichText(
+                      text: TextSpan(children: [
+                    TextSpan(
+                        text: 'How many competitors \n         would you like?',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        )),
+                  ])),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 50),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.13,
+            width: MediaQuery.of(context).size.width,
+            //color: Colors.amber,
+            child: Image.asset(
+              "assets/images/bar2.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.08,
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ChellengeFinal()));
+                },
+                color: blue.withOpacity(0.3),
+                child: Text(
+                  "NEXT",
+                  style: GoogleFonts.poppins(
+                    color: blue1,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )),
+          ),
+        ),
+      ]),
+    );
   }
 }
